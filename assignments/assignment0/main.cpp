e1c841b2f1079511a028eaa53e3a129a98119c85
Mat Mariani <someone.mariani@gmail.com>
2025-01-20 16:15:18 -0500

    Assignment0

--- assignments/assignment0/main.cpp
+++ assignments/assignment0/main.cpp
@@ -8,6 +8,13 @@
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
 
+#include "ew/camera.h"
+#include "ew/cameraController.h"
+#include "ew/shader.h"
+#include "ew/model.h"
+#include "ew/texture.h"
+#include "ew/transform.h"
+
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 GLFWwindow* initWindow(const char* title, int width, int height);
 void drawUI();
@@ -18,9 +25,66 @@ int screenHeight = 720;
 float prevFrameTime;
 float deltaTime;
 
+// camera
+ew::Camera camera;
+ew::CameraController cameraController;
+ew::Transform transform;
+GLuint texture;
+
+struct Material {
+	float Ka = 1.0; 
+	float Kd = 0.5; 
+	float Ks = 0.5;
+	float Shininess = 128;
+} material;
+
+
+void render(ew::Shader& shader, ew::Model& model)
+{
+	// describe pipeline
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+
+	// set bindings
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	shader.use();
+
+	// material properties
+	shader.setInt("texture0",0);
+	shader.setFloat("material.Ka", material.Ka);
+	shader.setFloat("material.Kd", material.Kd);
+	shader.setFloat("material.Ks", material.Ks);
+	shader.setFloat("material.Shininess", material.Shininess);
+
+	// camera
+	shader.setVec3("_EyePos", camera.position);
+
+	// scene matrices
+	auto viewproj = camera.projectionMatrix() * camera.viewMatrix();
+	shader.setMat4("model", transform.modelMatrix());
+	shader.setMat4("view_proj", viewproj);
+
+	// draw
+	model.draw();
+}
+
 int main() {
 	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	// initialize resources
+	ew::Shader blinnphong = ew::Shader("assets/blinnphong.vert", "assets/blinnphong.frag");
+	ew::Model suzanne = ew::Model("assets/Suzanne.obj");
+	texture = ew::loadTexture("assets/brick_color.jpg");
+
+	// initialize camera
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f);
+	camera.aspectRatio = (float)screenWidth/screenHeight;
+	camera.fov = 60.0f;
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -29,9 +93,17 @@ int main() {
 		deltaTime = time - prevFrameTime;
 		prevFrameTime = time;
 
+		// update the camera
+		cameraController.move(window, &camera, deltaTime);
+
+		// rotate suzanne
+		transform.rotation = glm::rotate(transform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
 		//RENDER
-		glClearColor(0.6f,0.8f,0.92f,1.0f);
+		glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		render(blinnphong, suzanne);
 
 		drawUI();
 
@@ -40,13 +112,30 @@ int main() {
 	printf("Shutting down...");
 }
 
+void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
+	camera->position = glm::vec3(0,0,5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+}
+
 void drawUI() {
 	ImGui_ImplGlfw_NewFrame();
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui::NewFrame();
 
 	ImGui::Begin("Settings");
-	ImGui::Text("Add Controls Here!");
+
+	if (ImGui::Button("Reset Camera")){
+		resetCamera(&camera, &cameraController);
+	}
+
+	if (ImGui::CollapsingHeader("Material")) {
+		ImGui::SliderFloat("AmbientK", &material.Ka, 0.0f, 1.0f);
+		ImGui::SliderFloat("DiffuseK", &material.Kd, 0.0f, 1.0f);
+		ImGui::SliderFloat("SpecularK", &material.Ks, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shininess", &material.Shininess, 2.0f, 1024.0f);
+	}
+
 	ImGui::End();
 
 	ImGui::Render();
@@ -73,6 +162,11 @@ GLFWwindow* initWindow(const char* title, int width, int height) {
 		printf("GLFW failed to init!");
 		return nullptr;
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
 	if (window == NULL) {
